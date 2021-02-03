2bb0b7f5782dbcbeda22388519e50b830dc58fc2
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-02-03 14:57:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform{
+    required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.8.0"
+    }
+  }
+ }
+
+# Configure the docker provider
+provider "docker" {
+}
+
+
+resource "docker_container" "terratomcat" {
+  image = "tomcat:latest"
+  name  = "tomcat"
+  restart = "always"
+  ports {
+    internal = 8080
+    external = 9090
+  }
+}
