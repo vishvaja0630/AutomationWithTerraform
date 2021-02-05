959e2022105defa604bea6149234dfa5cace881f
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-02-05 10:30:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform{
       source  = "kreuzwerker/docker"
       version = "2.8.0"
     }
-  }
+    }
  }
 
 # Configure the docker provider
 provider "docker" {
-
+#credentials for docker hub
   registry_auth {
     address = "registry.hub.docker.com"
     username = "vishvajafaldesai"
@@ -19,7 +19,7 @@ provider "docker" {
   }
 }
 
-
+#creation of image using dockerfile with name dockerisedtomcat and publish to docker hub id vishvajafaldesai
 resource "docker_image" "dockerisedtomcat" {
   name = "vishvajafaldesai/dockerisedtomcat:latest"
   build {
@@ -27,6 +27,7 @@ resource "docker_image" "dockerisedtomcat" {
   }
 }
 
+#creation of container dockerisedtomcatcontainer using image dockerisedtomcat and running the container on port 9093
 resource "docker_container" "dockerisedtomcatcontainer" {
   name  = "dockerisedtomcatcontainer"
   image = docker_image.dockerisedtomcat.latest
