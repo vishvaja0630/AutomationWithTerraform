9574972b6f934addd4159a7a4510447ef457958c
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-02-10 12:06:14 +0530

    Update deploy_tomcat.tf

--- aws/deploy_tomcat.tf
+++ aws/deploy_tomcat.tf
@@ -15,19 +15,10 @@ resource "aws_vpc" "main" {
     enable_dns_support = true
     enable_dns_hostnames = true
 
-     tags {
-         Name = "main"
-            }
-        }
-
  resource "aws_subnet" "subnet1" {
    vpc_id     = aws_vpc.main.id
    cidr_block = "10.0.1.0/24"
    availability_zone = var.availability_zone1
-
-  tags {
-    Name = "app-subnet-1"
-    }
   }
 
 
